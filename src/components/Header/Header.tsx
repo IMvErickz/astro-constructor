bfcdca2b2134996f0947300ee6cd80b378b97a33
IMvErickz <erickspy2003@gmail.com>
2024-07-12 15:05:23 -0300

    cicd

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+    return (
+        <header className="w-full flex items-center border-b border-b-solid border-b-slate-500 p-4">
+            <h1 className="text-white text-6xl font-bold">Feito com Astro</h1>
+        </header>
+    )
+}
---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,7 @@
+export function Main() {
+    return (
+        <main className="size-full flex flex-col items-center justify-center">
+            <h2 className="text-4xl font-semibold text-white">Só testando o CI/CD</h2>
+        </main>
+    )
+}
